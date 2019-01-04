25a56041a8fed07b1b0553c0c7cbefb0ea871a63
sdim-ua <ingersol@yandex.ua>
2019-01-04 14:00:26 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,19 @@
+
+/* 
+Мама мыла раму
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String a = "Мама";
+        String b = "Мыла";
+        String c = "Раму";
+        System.out.println(a + b + c);
+        System.out.println(c + a + b);
+        System.out.println(b + c + a);
+        System.out.println(a + c + b);
+        System.out.println(b + a + c);
+        System.out.println(c + b + a);
+    }
+}
